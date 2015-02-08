a744b50f1729a357f6156f2dd6517eb75ae005a2
ponyczek <1204969@rgu.ac.uk>
2015-02-08 17:46:03 +0000

    Devise added + migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203001106) do
+ActiveRecord::Schema.define(version: 20150203001311) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150203001106) do
     t.string   "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
